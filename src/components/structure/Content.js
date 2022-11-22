f00440f416345f38d22f8bb5cb045054bd1f6b44
ANTOROY10 <antoroy@karunya.edu.in>
2022-11-22 15:12:25 +0530

    Portfolio

---
+++ src/components/structure/Content.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AboutMe from "../sections/AboutMe";
+import Skills from "../sections/Skills";
+import Certifications from "../sections/Certifications";
+
+function Content() {
+  return (
+    <main>
+      <AboutMe />
+      <Skills />
+      <Certifications />
+    </main>
+  );
+}
+
+export default Content;
